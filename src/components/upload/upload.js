3dbdda639c59769b17bd4e62718c4e213ef6651e
Schiriac Robert <schiriac.robert@gmail.com>
2021-05-01 22:34:09 +0300

    kinda works

--- src/components/upload/upload.js
+++ src/components/upload/upload.js
@@ -1,60 +1,163 @@
-//import {useState} from "react";
+import { Component, createRef } from "react";
 import "./upload.css";
 
-const isDnDPossible = () => {
-  const div = document.createElement("div");
-  return [
-    "draggable" in div || ("ondragstart" in div && "ondrop" in div),
-    "FormData" in window,
-    "FileReader" in window,
-  ].every((v) => v);
-};
+class Upload extends Component {
+  constructor(props) {
+    super(props);
+    this.multiple = props.multiple || true;
+    this.fileTypes = props.fileTypes || ["png", "jpeg", "bmp", "jpg"];
 
-const Upload = ({
-  multiple = true,
-  fileTypes = ["png", "jpeg", "bmp", "jpg"],
-}) => {
-  const handleFiles = (e) => console.log(e);
-  const allowedFiles = fileTypes.map((ext) => `.${ext}`);
-  const formClasses = [
-    "dnd-container",
-    "rounded-lg",
-    "relative p-8",
-    "bg-gray-800",
-    "flex",
-    "flex-col",
-    "font-bold",
-    "text-center",
-    "cursor-pointer",
-    "justify-center",
-    isDnDPossible ? "border-dashed border-2" : "border-1",
-  ].join(" ");
+    this.dndContainerRef = createRef();
+    this.fileInputRef = createRef();
 
-  return (
-    <div className="p-4 relative flex flex-col">
-      <form className={formClasses}>
-        {isDnDPossible && (
-          <p className="p-2">Drag and drop multiple images in this area.</p>
-        )}
-        {!isDnDPossible && <p className="p-2">Select multiple images</p>}
-        <div className="opacity-60 text-sm">
-          Supported file types:
-          <div className="flex flex-row divide-x justify-center p-2">
-            {allowedFiles.map((ext) => (
-              <div className="px-6">{ext}</div>
-            ))}
+    this.state = {
+      formHighlight: false,
+      files: [],
+      previewFiles: [],
+    };
+  }
+
+  loadPreviewFiles = (files) => {
+    files.forEach((f) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(f);
+      reader.onloadend = () => {
+        this.setState((state) => ({
+          previewFiles: [...state.previewFiles, {
+            name: f.name,
+            data: reader.result
+          }],
+        }));
+      };
+    });
+  };
+
+  handleFiles = (files) => {
+    this.setState((state) => ({
+      files: [...state.files, ...files],
+    }));
+    this.loadPreviewFiles(files);
+  };
+
+  render = () => {
+    return (
+      <div className="p-4 relative flex flex-col">
+        <form ref={this.dndContainerRef} className={this.formClasses()}>
+          {this.isDnDPossible() && (
+            <p className="p-2">Drag and drop multiple files in this area.</p>
+          )}
+          {!this.isDnDPossible() && (
+            <p className="p-2">Select multiple files</p>
+          )}
+          <div className="opacity-60 text-sm">
+            Supported file types:
+            <div className="flex flex-row divide-x pointer-events-none justify-center">
+              {this.allowedFiles().map((ext) => (
+                <div key={ext} className="px-6">
+                  {ext}
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
-        <input
-          className="self-center p-6"
-          type="file"
-          hidden={!isDnDPossible}
-          multiple={multiple}
-          accept={allowedFiles}
-          onChange={handleFiles}
-        />
-      </form>
-    </div>
-  );
-};
+          <input
+            ref={this.fileInputRef}
+            className="self-center p-6"
+            type="file"
+            hidden={this.isDnDPossible()}
+            multiple={this.multiple}
+            accept={this.allowedFiles().join(", ")}
+            onChange={this.handleFiles}
+          />
+        </form>
+        {this.state.previewFiles.length > 0 && (<div>
+          {this.state.previewFiles.map(f => (
+            <img key={f.name} src={f.data} alt={f.name} />
+          ))}
+        </div>)}
+      </div>
+    );
+  };
+
+  componentDidMount = () => {
+    ["dragenter", "dragover", "dragleave", "drop"].forEach((e) => {
+      this.dndContainerRef.current.addEventListener(
+        e,
+        this.preventDefault,
+        false
+      );
+    });
+
+    ["dragenter", "dragover"].forEach((e) => {
+      this.dndContainerRef.current.addEventListener(
+        e,
+        () => this.dndState(true),
+        false
+      );
+    });
+
+    ["dragleave", "drop"].forEach((e) => {
+      this.dndContainerRef.current.addEventListener(
+        e,
+        () => this.dndState(false),
+        false
+      );
+    });
+
+    this.dndContainerRef.current.addEventListener(
+      "drop",
+      this.handleDrop,
+      false
+    );
+  };
+
+  handleDrop = (e) => {
+    const dt = e.dataTransfer;
+    const files = dt.files;
+    this.handleFiles(Array.from(files));
+  };
+
+  dndState = (formHighlight) => {
+    this.setState(() => ({
+      formHighlight,
+    }));
+  };
+
+  preventDefault = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+  };
+
+  isDnDPossible = () => {
+    const div = document.createElement("div");
+    return [
+      "draggable" in div || ("ondragstart" in div && "ondrop" in div),
+      "FormData" in window,
+      "FileReader" in window,
+    ].every((v) => v);
+  };
+
+  formClasses = () => {
+    return [
+      "rounded-lg",
+      "relative p-8",
+      "bg-gray-800",
+      "flex",
+      "flex-col",
+      "font-bold",
+      "text-center",
+      "cursor-pointer",
+      "justify-center",
+      this.isDnDPossible() ? "border-dashed border-2" : "border-1",
+      this.state.formHighlight
+        ? "ring-4 ring-indigo-200 border-gray-800"
+        : "border-white",
+    ]
+      .filter(Boolean)
+      .join(" ");
+  };
+
+  allowedFiles = () => {
+    return this.fileTypes.map((ext) => `.${ext}`);
+  };
+}
 export default Upload;
